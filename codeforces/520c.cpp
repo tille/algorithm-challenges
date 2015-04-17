a8f3ce686914debe2f4351b5d173a05cbdd4dc7f
Jorman Bustos <jbustos@altavistaed.com>
2015-04-17 11:46:25 -0500

    Add codeforces/520c.cpp.

--- codeforces/520c.cpp
+++ codeforces/520c.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <utility>
 
 using namespace std;
 
@@ -8,56 +9,48 @@ using namespace std;
 #define K 1000000007
 
 int letters[MAXN];
+int maxx, max_counter;
 
-int most_repeated_character(string s) {
-  int max = -1, 
-      max_counter = 1, 
-      letter_index;
+void most_repeated_character(string s) {
+  maxx = -1;
+  max_counter = 1;
+  int letter_index;
 
   for( int i = 0; i < s.length(); ++i ) {
     letter_index = int( s[i] );
     letters[letter_index] += 1;
 
-    if( letters[letter_index] > max ) {
+    if( letters[letter_index] > maxx ) {
       
-      max = letters[letter_index];
+      maxx = letters[letter_index];
       max_counter = 1;
     
-    } else if( letters[letter_index] == max ) {
+    } else if( letters[letter_index] == maxx ) {
       max_counter++;
     }
   }
-
-  return (max * max_counter);
 }
 
-int custom_potentiation(int m, int n) {
-  cout << m << "-" << n << endl;
-  int left_part, result;
-
+int cpot(int m, int n) {
   if( n%2 == 0 ) {
-    left_part = custom_potentiation(m, n/2);
-    result = custom_potentiation( left_part, 2 );
-  } else if( n == 1 ) {
-    result = m;
+    return (n == 2) ? ( m%K * m%K )%K : cpot(m, n/2);
   } else {
-    left_part = custom_potentiation(m, n-1);
-    result = ( ( left_part % K ) * ( m % K ) ) % K;
+    return (n == 1) ? m : (cpot(m, n-1)%K * m%K)%K;
   }
-
-  return result;
 }
 
 int main() {
-  int n, k;
+  int n, k, result;
   string s;
 
   memset(letters, 0, sizeof letters);
 
   cin >> n >> s;
 
-  k = most_repeated_character(s);
-  cout << custom_potentiation(k, n) << endl;
+  most_repeated_character(s);
+  result = (maxx==n) ? 1 : cpot(maxx*max_counter, n);
+
+  cout << result << endl;
 
   return 0;
 }
