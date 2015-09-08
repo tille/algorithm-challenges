83aa60083bdbd9df15925cd68ec7f87058b419c3
Jorman Bustos <tille100@gmail.com>
2015-09-08 14:46:01 -0500

    Add function to convert into binary system.

--- contests/col_icpc_2015/G.cpp
+++ contests/col_icpc_2015/G.cpp
@@ -13,14 +13,6 @@ complex<double> complex_pot( complex<double> n, int k ) {
   return result;
 }
 
-complex<double> mul_complex_k( complex<double> n, int k ) {
-  k--;
-  while ( k-- ) 
-    n += n;
-
-  return n;
-}
-
 complex<double> gx( string b ) {
   complex<double> CN (-1, 1);
   complex<double> acum (0, 0);
@@ -37,14 +29,29 @@ complex<double> gx( string b ) {
   return acum;
 }
 
+string to_binary( int n ) {
+  string result = "";
+  string binary_digit;
+
+  if ( n == 0 ) return "0";
+
+  while( n != 0 ) {
+    binary_digit = ( n%2 == 0 ) ? "0" : "1" ;
+    result = binary_digit + result;
+    n = n/2;
+  }
+
+  return result;
+}
+
 int main() {
   int p;
   string binary_num;
   complex<double> result;
 
   while( cin >> p ) {
-    binary_num = to_binary( n );
-    result = gx( "10" );
+    binary_num = to_binary( p );
+    result = gx( binary_num );
     cout << real(result) << " " << imag(result) << endl;
   }
 
