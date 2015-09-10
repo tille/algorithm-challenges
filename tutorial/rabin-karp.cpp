3c57fe87d0515bfb9d1b8d185a856319a5a9a68e
Jorman Bustos <tille100@gmail.com>
2015-09-10 08:24:29 -0500

    Add hash function implementation to rabin karp algorithm.

--- tutorial/rabin-karp.cpp
+++ tutorial/rabin-karp.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
 #include <string>
+#include <math.h>
 
 using namespace std;
 
-int rabin_karp(string text, string pattern) {
+int hash( string pattern ) {
+  double b = 101;
+  double m = pattern.size()-1;
+  int mirror_it;
+
+  for ( int it = 0; it < m; ++it ) {
+    mirror_it = int(m) - it;
+    hash_key += pattern[it]-'0' * int( pow(b, mirror_it) );
+  }
+
+  return hash_key;
+}
+
+int text_hash( string text, int i, int m ) {
+  int result;
+  if ( i == 0 ) 
+    result = hash( text.substr(0, m) );
+  else {
+    old_elm = text[i-m]-'0' * int( pow(b, mirror_it) );
+    result = (( text_keys[i-1] - old_elm ) * b) + text[i]-'0';
+  }
+
+  return text_keys[i] = result;
+}
+
+int rabin_karp( string text, string pattern ) {
   int found = 0,
       m = pattern.size(),
-      pattern_hash = hash(pattern);
+      pattern_hash = hash( pattern );
       chunk_hash;
 
-  for ( int i = 0; i+m < n; ++i ) {
-    chunk_hash = hash(text[i..i+m]);
+  for ( int i = 0; i+m <= text.size; ++i ) {
+    chunk_hash = text_hash( text, i, m );
     if ( chunk_hash == pattern_hash ) {
       found = 1;
       break;
