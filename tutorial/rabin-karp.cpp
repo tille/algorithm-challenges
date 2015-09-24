78dfdf63bc182e1992c11fd7da6395defea44952
Jorman Bustos <tille100@gmail.com>
2015-09-23 23:51:28 -0500

    Add rabin karp module fixes.

--- tutorial/rabin-karp.cpp
+++ tutorial/rabin-karp.cpp
@@ -5,32 +5,49 @@
 using namespace std;
 
 #define K 10000
+#define C 15000
 
 int text_keys[K];
 
+// correctly calculates a mod b even if a < 0
+int int_mod(int a, int b) {
+  return (a % C + C) % C;
+}
+
+int custom_pot(int x, int n) {
+  int result = 1;
+  
+  while( n-- )
+    result = (result * (x % C)) % C;
+
+  return result;
+}
+
 int hashing( string pattern ) {
-  double b = 101;
-  double m = pattern.size()-1;
+  int b = 101;
+  int m = pattern.size()-1;
   int mirror_it;
   int hash_key = 0;
+  int new_value;
 
   for ( int it = 0; it <= m; ++it ) {
-    mirror_it = int( m ) - it;
-    hash_key += int( pattern[it] ) * int( pow( b, mirror_it ) );
+    mirror_it = m - it;
+    hash_key += (int(pattern[it]) % C * custom_pot(b, mirror_it) % C) % C;
   }
 
-  return hash_key;
+  return hash_key % C;
 }
 
 void fill_text_keys( string text, int m ) {
-  double b = 101;
+  int b = 101;
 
   for( int it = 0; it+m <= text.size(); ++it ) {
     if ( it == 0 ) 
       text_keys[0] = hashing( text.substr(0, m) );
     else {
-      int old_elm = text[it-1] * int( pow(b, m-1) );
-      text_keys[it] = (( text_keys[it-1] - old_elm ) * b) + int( text[it+m-1] );
+      int old_elm = (int(text[it-1]) * custom_pot(b, m-1) % C) % C;
+      // cout << it << "- " << text_keys[it-1] - old_elm << endl; 
+      text_keys[it] = (( int_mod(text_keys[it-1] - old_elm, C) * b) % C + int( text[it+m-1] )) % C;
     }
   }
 }
@@ -62,7 +79,8 @@ int main() {
   string text = "((XY(XYX))Y((XY(XYX))YX))";
 
   int found = rabin_karp(text, pattern);
-  cout << "pattern found on the text: " << found << endl;
+  cout << text_keys[1] << endl;
+  // cout << "pattern found on the text: " << found << endl;
   
   return 0;
 }
