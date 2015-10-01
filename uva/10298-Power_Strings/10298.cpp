fd90f6a75e1ddb2b2fc1ea5e80c86313a7e196d1
Jorman Bustos <tille100@gmail.com>
2015-10-01 16:04:01 -0500

    Update 10298.cpp

--- uva/10298-Power_Strings/10298.cpp
+++ uva/10298-Power_Strings/10298.cpp
@@ -36,3 +36,15 @@ int main() {
 
   return 0;
 }
+
+// este problema tiene una solucion O(1) despues de hacer la tabla de kmp
+// se encuentra el minimo periodo k del string s que es k = n - f[n-1]
+// luego se verifica que k este un numero entero de veces en la cadena
+// es decir: (n%k==0) ? n/k : 1
+
+// la solucion actual verifica si el prefijo/sufijo mas largo de la cadena es menor 
+// que la longitud del string divida 2, entonces: con ese prefijo no se puede formar la cadena
+// si la cadena puede ser formada es decir tiene un prefijo/sufijo igual o mayor que la 
+// mitad del string, entonces: si el prefijo es igual quiere decir que ans = n / prefijo.size
+// mientras prefijo[i] > 0 y prefijo[i] sean iguales o mayores que i/2 se debe continuar verificando
+// la solucion es la mayor de estos resultados.
