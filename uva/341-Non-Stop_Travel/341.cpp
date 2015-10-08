46680e15f36a3695db70d7592a080885dfdd0914
Jorman Bustos <tille100@gmail.com>
2015-10-08 16:40:50 -0500

    dijkstra: uva: fix solution to 341 - Non-Stop Travel.
    dist[0] = 0 is required.

--- uva/341-Non-Stop_Travel/341.cpp
+++ uva/341-Non-Stop_Travel/341.cpp
@@ -14,6 +14,7 @@ void dijkstra(int s) {
   }
 
   priority_queue < edge, vector<edge>, greater<edge> > q;
+  d[s] = 0;
   q.push(edge(0, s));
 
   while (q.size()) {
@@ -21,11 +22,11 @@ void dijkstra(int s) {
     int u = q.top().second; 
     q.pop();
     
-    if (d[u] <= dist) continue;
+    if (d[u] < dist) continue;
 
     for (int i = 0; i < g[u].size(); ++i) {
-      int weight = g[u][i].first;
-      int v = g[u][i].second;
+      int v = g[u][i].first;
+      int weight = g[u][i].second;
 
       if ((dist + weight) < d[v]) {
         d[v] = dist + weight;
@@ -57,21 +58,41 @@ void clear_graph() {
 
 int main() {
   int test_case = 0, n, to, delay, from, r;
-  clear_graph();
+  vector<int> path;
 
   while( cin >> n && n) {
+    test_case++;
+    clear_graph();
+    path.clear();
+
     for (int i = 0; i < n; ++i) {
       cin >> r;
 
       while (r--) {
         cin >> to >> delay;
-        g[i].push_back( edge(delay, to) );
+        g[i].push_back( edge(to-1, delay) );
       }
     }
 
     cin >> from >> to;
-    dijkstra(from-1);
-    cout << d[to-1] << endl;
+    from--; to--;
+    dijkstra(from);
+
+    int t = to;
+    path.push_back(t+1);
+
+    while (p[t] != -1) {
+      path.push_back(p[t]+1);
+      t = p[t];
+    }
+    reverse(path.begin(), path.end());
+
+    printf("Case %d: Path = ", test_case);
+    for (int i = 0; i < path.size(); ++i) {
+      printf("%d", path[i]);
+      if (i != path.size()-1) printf(" ");
+    }
+    printf("; %d second delay\n", d[to]);
   }
   return 0;
 }
