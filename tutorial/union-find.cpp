d1230680e9d524e0054169b8c0e06a84c4548458
Jorman Bustos <tille100@gmail.com>
2015-10-27 20:53:01 -0500

    tutorial: union-find: update union find implementation.

--- tutorial/union-find.cpp
+++ tutorial/union-find.cpp
@@ -1,21 +1,16 @@
-#include <iostream>
-
-using namespace std;
-
+// initialize each node in its own set
 void init(int n){
-	for(int i = 0; i < n; ++i) p[i] = i;
+  for(int i = 0; i < n; ++i) p[i] = i;
 }
 
+// update members of a set and find its set id
 int find(int u){
-	return p[u]==u?u:p[u]=find(p[u]);
+  return p[u] == u ? u : p[u] = find(p[u]);
 }
 
+// join two sets
 void link(int u, int v){
-	int a = find(u);
-	int b = find(v);
-	if(a!=b) p[a] = b;
-}
-
-int main(){
-	return 0;
+  int a = find(u);
+  int b = find(v);
+  if (a != b) p[a] = b;
 }
