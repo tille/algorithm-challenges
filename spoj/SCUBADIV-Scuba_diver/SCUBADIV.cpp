dfcd9e5a45c0ceeebba568ddf7d25c9066e639ec
Jorman Bustos <tille100@gmail.com>
2015-10-27 20:11:33 -0500

    dp: knapsack: spoj: add Scuba diver problem.
    3 dimensional dp, here we go.

--- spoj/SCUBADIV-Scuba_diver/SCUBADIV.cpp
+++ spoj/SCUBADIV-Scuba_diver/SCUBADIV.cpp
@@ -2,34 +2,51 @@
 
 using namespace std;
 
-int main() {
-  int inf = 1<<30;
-  int n;
-  int W;
-  int w[100];
-  int v[100];
+#define MAXN 1002
+#define MAX_O 23
+#define MAX_N 81
 
-  cin >> W >> n;
+int h20[MAXN];
+int nitro[MAXN];
+int w[MAXN];
+int dp[MAXN][MAX_O][MAX_N];
+
+void knapsack(int n, int size_o, int size_n) {
+  for (int j = 0; j <= size_o; ++j) 
+    for (int k = 0; k <= size_n; ++k) 
+      dp[0][j][k] = 1<<30;
+
+  dp[0][0][0] = 0;
+
   for (int i = 1; i <= n; ++i) {
-    cin >> w[i] >> v[i];
-  }
+    for (int j = 0; j <= size_o; ++j) {
+      for (int k = 0; k <= size_n; ++k) {
+        int no_taken = dp[i-1][j][k];
+        int taken = 
+          dp[i-1][max(0, j-h20[i])][max(0, k-nitro[i])] + w[i];
 
-  int max_n, max_w; max_n = max_w = 100;
-
-  int dp[max_n][max_w]; 
-
-  dp[0][0] = 0;
-  for (int i = 1; i <= W; ++i) dp[0][i] = inf;
-
-  for (int i = 1;  i <= n; ++i) {
-    for (int j = 0; j <= W; ++j) {
-      dp[i][j] = dp[i-1][j];
-      if (j-w[i] >= 0 && dp[i-1][j-w[i]] != inf) {
-        dp[i][j] = min(dp[i][j], dp[i-1][j-w[i]] + v[i]);
+        dp[i][j][k] = min(no_taken, taken);
       }
     }
   }
+}
 
-  cout << dp[n][W] << endl;
+int main() {
+  int size_o, size_n, t, n;
+
+  cin >> t;
+
+  while (t--) {
+    cin >> size_o >> size_n;
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+      cin >> h20[i] >> nitro[i] >> w[i];
+    }
+    
+    knapsack(n, size_o, size_n);
+
+    cout << dp[n][size_o][size_n] << endl;
+  }
   return 0;
 }
