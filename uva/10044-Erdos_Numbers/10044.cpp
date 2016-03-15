da88c07052184ffbc622c238ac6e9ab676972714
Jorman Bustos <tille100@gmail.com>
2016-03-15 16:35:58 -0500

    uva: bfs: Add problem 10044 - Erdos Numbers.

--- uva/10044-Erdos_Numbers/10044.cpp
+++ uva/10044-Erdos_Numbers/10044.cpp
@@ -3,114 +3,134 @@
 using namespace std;
 
 typedef long long ll;
-typedef vector<int> vi;
 typedef pair<int, int> ii;
 typedef vector<ii> vii; 
-typedef set<int> si; 
-typedef map<string, int> msi;
 
-#define REP(i, a, b) \
-  for (int i = int(a); i <= int(b); i++)
-#define TRmsi(c, it) \
-  for (si::iterator it = (c).begin(); it != (c).end(); it++)
-#define MAXN 10000
+#define REP(i, a, b) for (int i = int(a); i <= int(b); i++)
+#define TRmsi(c, it) for (msi::iterator it = (c).begin(); it != (c).end(); it++)
 
-int t, p, n, cont;
-string line, name, surname, author;
-vector<int> v;
-msi authors;
-si g[MAXN];
-int visited[MAXN];
-int sol[MAXN];
+int t, p, n, aa;
+string line;
+vector<int> g[1000000];
+map<string, int> pos;
+set<ii> vis;
+int ans[1000000];
 
-void bfs(int start, int mark) {
+string format(const string &s) {
+  int i = 0; int j = s.size() - 1;
+  while (s[i] == ' ') i++;
+  while (s[j] == ' ') j--;
+  return s.substr(i, j - i + 1);
+}
+
+void add_graph(const string &s) {
+  vector<string> authors;
+  int p = 0, comma = 0;
+  for (int i = 0; i < s.size(); ++i) {
+    if (s[i] == ',') comma++;
+    if (s[i] == ':' || comma == 2) {
+      string author = format(s.substr(p, i - p));
+      if (author.size()) {
+        authors.push_back(author);
+        if (!pos[author]) {
+          // if (author == "Erdos, P.") cout << "fdsa" << endl;
+          pos[author] = aa++;
+        }
+      }
+      p = i + 1;
+      comma = 0;
+    }
+  }
+
+  REP(i, 0, authors.size() - 1) {
+    int cur = pos[authors[i]];
+    REP(j, i + 1, authors.size() - 1) {
+      int to = pos[authors[j]];
+      ii state = make_pair(to, cur);
+
+      if (!vis.count(state)) {
+        g[cur].push_back(to);
+        g[to].push_back(cur);
+        vis.insert(make_pair(cur, to));
+        vis.insert(make_pair(to, cur));
+      }
+    }
+  }
+}
+
+void bfs(int ini) {
   queue<ii> q;
-  q.push( make_pair(start, mark) );
+  q.push(make_pair(ini, 0));
 
   while (q.size()) {
     int cur = q.front().first;
-    mark = q.front().second;
+    int d = q.front().second;
     q.pop();
 
-    if (visited[cur]) continue;
-    visited[cur] = 1;
-    sol[cur] = mark;
-
-    TRmsi(g[cur], next)
-      if (!visited[*next])
-        q.push( make_pair(*next, sol[cur] + 1) );
-  }
-}
-
-void clear_all() {
-  authors.clear(); cont = 0;
-  REP(j, 0, MAXN - 2) {
-    visited[j] = 0;
-    sol[j] = -1;
-    g[j].clear();
-  }
-}
-
-int line_pos;
-string next_author() {
-  int cant = 0, idx;
-  while (true) {
-    cant++; idx = line_pos + cant;
-    if ( (line[idx] == '.' && (idx + 1) < (line.size() - 1) && line[idx + 1] == ',') || line[idx] == ':') {
-      string ans = line.substr(line_pos, cant);
-      line_pos = (line[idx] == ':') ? -1 : line_pos + cant + 3;
-      return ans;
+    if (ans[cur] == -1) {
+      ans[cur] = d;
+      for (int j = 0; j < g[cur].size(); ++j) {
+        int to = g[cur][j];
+        q.push(make_pair(to, d + 1));
+      }
     }
   }
 }
 
 int main() {
-  scanf("%d", &t);
-  REP(i, 0, t - 1) {
-    clear_all();
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+  cin >> t;
+  aa = 1;
+
+  REP(tc, 1, t) {
     cin >> p >> n;
-    getline(cin, line); // read endline
+    getline(cin, line);
+    pos.clear();
+    vis.clear();
 
-    REP(j, 0, p - 1) {
-      v.clear();
-      // read authors and assign ids for them
-      getline(cin, line);
-      line_pos = 0;
-
-      do {
-        author = next_author();
-        if (authors.count(author) == 0) authors[author] = cont++;
-        v.push_back(authors[author]);
-        if (line_pos == -1) break;
-      } while (true);
-
-      // create graph
-      REP(k, 0, v.size() - 1) REP(r, 0, v.size() - 1) {
-        if (k == r) continue;
-        g[v[k]].insert(v[r]);
-        g[v[r]].insert(v[k]);
-      }
+    REP(i, 0, aa) {
+      g[i].clear();
     }
-    bfs(authors["Erdos, P."], 0);
 
-    // REP(k, 0, cont - 1) {
-    //   cout << k << ": "; 
-    //   TRmsi(g[k], it) {
-    //     cout << (*it) << " ";
+    aa = 1;
+    REP(i, 1, p) {
+      getline(cin, line);
+      add_graph(line);
+    }
+
+    REP(i, 0, aa) {
+      ans[i] = -1;
+    }
+    // REP(i, 1, aa-1) {
+    //   ans[i] = -1;
+    //   REP(j, 0, g[i].size() - 1) {
+    //     cout << g[i][j] << " ";
     //   }
     //   cout << endl;
     // }
-    // REP(j, 0, cont - 1)
-    //   cout << sol[j] << endl;
 
-    cout << "Scenario " << i + 1 << endl;
-    REP(j, 0, n - 1) {
-      getline(cin, name);
-      cout << name << " ";
-      int mark = sol[authors[name]];
-      if (mark == -1) cout << "infinity" << endl;
-      else cout << mark << endl;
+    bfs(pos["Erdos, P."]);
+    // cout << "blah: " << pos["Erdos, P."] << endl;
+    // for(map<string,int>::iterator it = pos.begin(); it != pos.end(); ++it) {
+    //   cout << it->first << "\n";
+    // }
+
+    // REP(i, 1, aa-1) {
+    //   cout << ans[i] << endl;
+    // }
+
+    string author;
+    cout << "Scenario " << tc << endl;
+    REP(j, 1, n) {
+      getline(cin, author);
+      int dist = ans[pos[author]];
+      if (dist != -1) {
+        cout << author << " " << dist << endl;
+      } else {
+        cout << author << " " << "infinity" << endl;
+      }
     }
   }
   return 0;
 }
+
